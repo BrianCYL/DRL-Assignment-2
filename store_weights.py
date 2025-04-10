5c9d4d7edaae6554f2f4e0227905d0fa38fb739d
chunyulin <r12942148@ntu.edu.tw>
2025-04-11 01:45:39 +0800

    85000 eps approximator

---
+++ store_weights.py
@@ -0,0 +1,39 @@
+import pickle
+import os
+import numpy as np
+from utils import NTupleApproximator
+# import json
+
+def save_weights(approximator, filename):
+    weights = []
+    for i in range(len(approximator.weights)):
+        weights.append(approximator.weights[i])
+    with open(filename, 'wb') as f:
+        pickle.dump(weights, f)
+    print(f"Weights saved to {filename}")
+
+def load_weights(filename):
+    with open(filename, 'rb') as f:
+        weights = pickle.load(f)   
+    return weights
+
+def main():
+    file_name = "../model/value_after_state_approximator_85000.pkl"
+    # Assume: self.weights = [defaultdict(float), defaultdict(float), ...]
+    # Convert to list of dicts
+    # Load the approximator
+    with open(file_name, 'rb') as f:
+        approximator = pickle.load(f)
+    
+    for w in approximator.weights:
+       for k, v in w.items():
+           if isinstance(v, np.float64):
+               w[k] = float(v)
+    
+    with open("value_approximator_85000.pkl", 'wb') as f:
+        pickle.dump(approximator, f)
+
+
+
+if __name__ == "__main__":
+    main()
